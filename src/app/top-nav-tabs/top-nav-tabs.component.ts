0b39dea35f45ffdf4c0d626f3b250344df89ba6f
kareemyasser8 <kareem.yasserr@gmail.com>
2023-09-05 12:48:48 +0200

    Add nav top time tabs

--- src/app/top-nav-tabs/top-nav-tabs.component.ts
+++ src/app/top-nav-tabs/top-nav-tabs.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavTabsComponent implements OnInit {
 
+  tabs: string[] = ['Today', 'Yesterday', 'Last week'];
+  selectedTab: number = 0; // Initially select the first tab
+
+  selectTab(index: number) {
+    this.selectedTab = index;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
