3266ce917a51247d2064931b0444b8ad9e9c39a3
kareemyasser8 <kareem.yasserr@gmail.com>
2023-09-06 16:41:14 +0200

    Finish Sales by Station

--- src/app/list-options-pop-up/list-options-pop-up.component.ts
+++ src/app/list-options-pop-up/list-options-pop-up.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-options-pop-up',
   templateUrl: './list-options-pop-up.component.html',
-  styleUrls: ['./list-options-pop-up.component.css']
+  styleUrls: ['./list-options-pop-up.component.css'],
 })
 export class ListOptionsPopUpComponent implements OnInit {
 
-  constructor() { }
+  options = [
+    { checked: true, text: 'Total Sales' },
+    { checked: false, text: 'Max Transaxction count per hour' },
+    { checked: true, text: 'Total Transaction' },
+    { checked: false, text: 'Max Transaction count date-time' },
+    { checked: true, text: 'Total Value' },
+    { checked: false, text: 'Compressor status' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
